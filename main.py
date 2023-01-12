35302e68ac97df025672b0a381d4adc8e2736018
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2023-01-12 20:10:26 +0200

    whatsapp

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pywhatkit
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def send_message_inst():
+    mobile = input('Введите номер получателя: ')
+    # mobile = os.getenv('MOBILE')
+    message = input('Введите текст сообщения: ')
+
+    pywhatkit.sendwhatmsg_instantly(phone_no=mobile, message=message)
 
 
-# Press the green button in the gutter to run the script.
+def send_message():
+    mobile = input('Введите номер получателя: ')
+    # mobile = os.getenv('MOBILE')
+    message = input('Введите текст сообщения: ')
+    hour = int(input('Введите часы: '))
+    minutes = int(input('Введите минуты: '))
+
+    pywhatkit.sendwhatmsg(phone_no=mobile, message=message, time_hour=hour, time_min=minutes)
+
+
+def main():
+    send_message_inst()
+    # send_message()
+    pywhatkit.image_to_ascii_art(img_path='1601_oooo.plus.png')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
